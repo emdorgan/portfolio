24845a9f140b7a0dc79d6090f5d05940a52b291d
Emily <wmdorgan@gmail.com>
2023-10-23 11:28:28 -0700

    feat: add assets and change descriptions

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,18 +8,21 @@ const About = () => {
     return (
         <Container maxWidth={false} disableGutters  sx={{marginTop: '120px', marginLeft: 0, marginRight: 0}}>
             <Grid container spacing={2}>
-                <Grid xs={12}>
-                    <Typography variant='h4' sx={{margin: '0.5em', float: 'left', borderBottom: 1, borderColor: 'white'}}>About Me</Typography>
+                <Grid xs={1}></Grid>
+                <Grid xs={5}>
+                    <Typography align='left' variant='h4' sx={{margin: '0.5em'}}>About Me</Typography>
+                    <Typography align='left' sx={{margin: '1em', textIndent: '3em'}}>
+                        I was born in Holland, grew up in France and I currently live in the bay area with my partner and our two cats.
+                        I graduated from Rensselaer Polytechnic Institute with a Bachelor's of Science in Design Innovation & Society with a minor in sustainability studies.
+                        For a couple of years, I worked as a Computer Science and English teacher. Due to the high cost of living in the bay area, I also worked for Zesty, a catering startup that was eventually bought out by Caviar.
+                        The pandemic decimated that job, and gave me the opportunity to finally change my career. In order to get a quick start, I completed the Full Stack Coding Bootcamp at UC Berkeley.
+                        I quickly realized that this was a very good fit for me, and my design background ended up helping out a surprising amount; all three of the group projects were ideas that I had initially pitched. 
+                        Following my graduating, I was hired by Soliant Consulting where I developed custom software solutions for a variety of small and medium businesses. Due to the consulting nature of our business, I not only developed software but I also interacted with clients
+                        and was involve in the foundation phase alongside our trusted Business Analysts and Technical Leads. I am currently seeking employment and working on a freelance project.
+                    </Typography>
                 </Grid>
-                <Grid xs={12}>
-                    <Typography sx={{margin: '1em', float: 'left'}}>I'm a full stack web developer with a background in design. I was born in Holland, grew up in France and moved to the United States for college. I currently live in the bay area with my partner and our two cats</Typography>
-                </Grid>
-                <Grid xs={12}>
-                    <Typography variant='h4' sx={{margin: '0.5em', float: 'left', borderBottom: 1, borderColor: 'white'}}>Work Experience</Typography>
-                </Grid>
-                <Grid xs={12}>
-                    <Typography sx={{margin: '1em', float: 'left'}}>In 2021, I was hired by Soliant Consulting, where we developed custom software solutions for small and medium businesses. My responsibilities included </Typography>
-                </Grid>
+                <Grid xs={5}></Grid>
+                <Grid xs={1}></Grid>
             </Grid>
         </Container>
     );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,39 +1,30 @@
 import { Button, Container, List, ListItem, Typography } from "@mui/material"
 import Grid from '@mui/material/Unstable_Grid2';
 
-const youtubeLinkAboutNeedForThisCalendar = 'https://www.youtube.com/watch?v=xAzA7IvOqGs';
+//@TODO add technologies used
+//@TODO for the e-sports calendar, describe challenges
 
 const Projects = () => {
     return (
         <Container maxWidth={false} disableGutters  sx={{marginTop: '120px', marginLeft: 0, marginRight: 0}}>
-            <Grid container spacing={2}>
-                <Grid xs={1}></Grid>
-                <Grid xs={10}>
-                    <Typography variant='h4' sx={{margin: '0.5em', borderBottom: 1, borderColor: 'white'}}>Current Project: Dynamic Esports Event Calendar</Typography>
-                </Grid>
-                <Grid xs={1}></Grid>
-                <Grid xs={2}></Grid>
-                <Grid xs={8}>
+            <Grid container spacing={0}>
+                <Grid md={1} sm={0}></Grid>
+                <Grid md={10} sm={12}>
+                    <Typography align='left' variant='h4' sx={{margin: '0.5em'}}>Current Project: Dynamic Esports Event Calendar</Typography>
+
                     <Typography align='left' sx={{margin: '1em', textIndent: '3em'}}>
                         The problem: The Age of Empires II community is a vibrant gaming community with a grass-roots competitive scene. Most tournaments are sponsored by fans of the game and the range of tournaments include 4 different 'tiers'. 
                         This means that, in 2023 alone there were over 100 tournaments which makes it very confusing and complex to navigate.
                     </Typography>
                     <Typography align='left' sx={{margin: '1em', textIndent: '3em'}}>
                         The solution: Using cheerio to parse tournament data from Liquidpedia, I developed a Node.js API that returns a list of all tournaments organized by tier. Data from my API is then sent to a 
-                        React Front end that displays each tournament in a calendar using react-big-calendar. Note that this project is ongoing and subject to change based on feedback from the community.
+                        React Front end that displays each tournament in a calendar using react-big-calendar. Note that this project is ongoing and subject to frequent change.
                     </Typography>
                     <Button variant='contained' sx={{m:2}} onClick={()=> window.open('https://github.com/emdorgan/aoe-resources')}>Front End Repository</Button>
                     <Button variant='contained' sx={{m:2}} onClick={()=> window.open('https://github.com/emdorgan/aoe-resources-api')}>Back End Repository</Button>
-                </Grid>
-                <Grid xs={2}></Grid>
-                <Grid xs={1}></Grid>
-                <Grid xs={10}>
-                    <Typography variant='h4' sx={{margin: '0.5em', borderBottom: 1, borderColor: 'white'}}>My work at Soliant Consulting</Typography>
-                </Grid>
-                <Grid xs={1}></Grid>
-                <Grid xs={2}></Grid>
-                <Grid xs={8}>
-                <Typography align='left' sx={{margin: '1em', textIndent: '3em'}}>
+                    <Typography align='left' variant='h4' sx={{margin: '0.5em'}}>My work at Soliant Consulting</Typography>
+
+                    <Typography align='left' sx={{margin: '1em', textIndent: '3em'}}>
                         As an Associate Cloud Native Application Developer, it was my responsibility to create custom full stack websites for our clients.
                         The tech stack was most often a React Front end and a Node.js backend featuring MUI, React-Hook-Form, Tanstack, MikroORM and PostgreSQL.
                         However due to the nature of our clients having their own infrastucture and needs, the tools we used varied extensively. Projects I was involved in included:
@@ -45,7 +36,7 @@ const Projects = () => {
                         <ListItem>- A QR 'badge' system for students to check into activity kiosks</ListItem>
                     </List>
                 </Grid>
-                <Grid xs={2}></Grid>
+                <Grid md={1} sm={0}></Grid>
             </Grid>
         </Container>
     );
